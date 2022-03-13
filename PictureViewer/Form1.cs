62c4979769feb7d2773981be31fccbdd422c8108
Iffahan Suksuwan <jackcopsluncher@gmail.com>
2022-03-13 21:17:47 +0700

    open Directory alblum and Show in PictureBox

--- PictureViewer/Form1.cs
+++ PictureViewer/Form1.cs
@@ -2,9 +2,28 @@ namespace PictureViewer
 {
     public partial class PictureViewer : Form
     {
+        int counter = 1;
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+        private OpenFileDialog openFileDialog1;
+        string[] images;
+
         public PictureViewer()
         {
             InitializeComponent();
+        }
+
+        private string[] GetFiles(string folderPath, String Searchpattern)
+        {
+            List<string> files = new List<String>();
+            var patterns = Searchpattern.Split('|');
+
+            foreach (var pattern in patterns)
+            {
+                string[] temps = Directory.GetFiles(folderPath, pattern);
+                files.AddRange(temps);
+            }
+
+            return files.ToArray();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -14,7 +33,14 @@ namespace PictureViewer
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            DialogResult result = fbd.ShowDialog();
 
+            images = GetFiles(fbd.SelectedPath, "*.gif|*.jpg|*.png|*.bmp");
+            System.Diagnostics.Debug.WriteLine(result.ToString());
+            System.Diagnostics.Debug.WriteLine(images.Length);
+            System.Diagnostics.Debug.WriteLine(fbd.SelectedPath.ToString());
+
+            pictureBox1.Image = Image.FromFile(images[counter]);
         }
 
         private void NextButton_Click(object sender, EventArgs e)
